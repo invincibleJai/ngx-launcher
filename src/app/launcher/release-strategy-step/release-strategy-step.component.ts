38d59bcc015d3502cf43baa993f5ac3d7ce4ca83
invinciblejai <mailjai.vardhan@gmail.com>
2018-02-12 17:07:41 +0530

    pipeline service integration and related changes
    Signed-off-by: invinciblejai <mailjai.vardhan@gmail.com>

--- src/app/launcher/release-strategy-step/release-strategy-step.component.ts
+++ src/app/launcher/release-strategy-step/release-strategy-step.component.ts
@@ -78,7 +78,13 @@ export class ReleaseStrategyStepComponent extends WizardStep implements OnInit, 
 
     this.wizardComponent.addStep(this);
     this.subscriptions.push(this.pipelineService.getPipelines().subscribe((result) => {
-      this._pipelines = this.allPipelines = result;
+      // needs to filter out associated pipelines from list of pipelines
+      let selPipelines: any[] = [];
+      selPipelines = result.filter(item => {
+        return item.platform == "maven";
+      })
+
+      this._pipelines = this.allPipelines = selPipelines;
       for (let i = 0; i < this._pipelines.length; i++) {
         if (this._pipelines[i].suggested === true) {
           this._pipelines[i].expanded = true;
@@ -216,7 +222,7 @@ export class ReleaseStrategyStepComponent extends WizardStep implements OnInit, 
     }
     this.pipelineId = selection.pipelineId;
     for (let i = 0; i < this.pipelines.length; i++) {
-      if (this.pipelineId === this.pipelines[i].pipelineId) {
+      if (this.pipelineId === this.pipelines[i].id) {
         this.wizardComponent.summary.pipeline = this.pipelines[i];
       }
     }
